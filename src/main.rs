83605442c223a261164b4d8e8c2d8330e6878ee0
tomon9086 <tomon9086@gmail.com>
2022-06-16 15:56:34 +0900

    tokenize "def" "extern" kw and identifiers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::{
     env, error, fmt, fs,
     io::{self, BufRead},
-    path, result,
+    path, result, str,
 };
 
 type Result<T> = result::Result<T, Error>;
+type Lines = io::Lines<io::BufReader<fs::File>>;
 
 #[derive(Debug, Clone)]
 struct Error {
@@ -45,12 +46,81 @@ fn read_options() -> Result<CmdOption> {
     Ok(CmdOption { filepath: filepath })
 }
 
-fn read_file_by_lines<P>(path: P) -> io::Result<io::Lines<io::BufReader<fs::File>>>
+fn read_file_by_lines<P>(path: P) -> io::Result<Lines>
 where
     P: AsRef<path::Path>,
 {
     let file = fs::File::open(path)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+enum Token {
+    TokEof = -1,
+
+    // commands
+    TokDef = -2,
+    TokExtern = -3,
+    // primary
+    TokIdentifier = -4,
+    TokNumber = -5,
+}
+
+fn get_char(chars: &mut str::Chars) -> Result<char> {
+    let c = chars.next();
+    match c {
+        Some(c) => Ok(c),
+        None => Err(Error::new("eol")),
+    }
+}
+
+fn is_space(c: char) -> bool {
+    c.is_whitespace()
+}
+
+fn is_alpha(c: char) -> bool {
+    c.is_alphabetic()
+}
+
+fn is_alnum(c: char) -> bool {
+    c.is_alphanumeric()
+}
+
+fn tokenize(chars: &mut str::Chars) -> Token {
+    let mut identifier_str: String; // Filled in if tok_identifier
+    let num_val: f64; // Filled in if tok_number
+    let mut last_char = ' ';
+
+    let mut gc = || match get_char(chars) {
+        Ok(c) => c,
+        Err(_) => '\0',
+    };
+
+    // Skip any whitespace.
+    while is_space(last_char) {
+        last_char = gc();
+    }
+
+    if is_alpha(last_char) {
+        identifier_str = last_char.to_string();
+
+        last_char = gc();
+        while is_alnum(last_char) {
+            identifier_str += &last_char.to_string();
+            last_char = gc();
+        }
+
+        if identifier_str == "def" {
+            return Token::TokDef;
+        }
+        if identifier_str == "extern" {
+            return Token::TokExtern;
+        }
+        println!("\"{}\"", identifier_str);
+        return Token::TokIdentifier;
+    }
+
+    Token::TokEof
 }
 
 fn main() {
@@ -64,9 +134,17 @@ fn main() {
     let filepath = options.filepath;
 
     if let Ok(lines) = read_file_by_lines(filepath) {
-        for (i, line) in lines.enumerate() {
-            if let Ok(ip) = line {
-                println!("{}: {}", i, ip);
+        for l in lines {
+            // let mut chars = line.expect("lines failed").chars();
+            let line = match l {
+                Ok(line) => line,
+                Err(_) => String::new(),
+            };
+            let mut chars = line.chars();
+
+            while !chars.as_str().is_empty() {
+                let token = tokenize(&mut chars);
+                println!("{:?}", token);
             }
         }
     }
