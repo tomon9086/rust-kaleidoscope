c8d5c2853c24e54a1ec05c621e30aae245665ce7
tomon9086 <tomon9086@gmail.com>
2022-07-22 13:35:39 +0900

    lexer peeking chars

--- src/lexer.rs
+++ src/lexer.rs
@@ -23,6 +23,15 @@ fn get_char(chars: &mut str::Chars) -> Result<char> {
     }
 }
 
+fn peek_char(chars: &str::Chars) -> char {
+    let mut peekable = chars.clone().peekable();
+    let c = peekable.peek();
+    match c {
+        Some(c) => *c,
+        None => EOF,
+    }
+}
+
 fn is_space(c: char) -> bool {
     c.is_whitespace()
 }
@@ -43,40 +52,43 @@ fn is_digit(c: char) -> bool {
 pub fn tokenize(chars: &mut str::Chars) -> i32 {
     let mut identifier_str: String; // Filled in if tok_identifier
     let num_val: f64; // Filled in if tok_number
-    let mut last_char = ' ';
-
-    let mut gc = || get_char(chars).unwrap_or(EOF);
+    let mut last_char;
+    let mut peeked_char = peek_char(chars);
 
     // Skip any whitespace.
-    while is_space(last_char) {
-        last_char = gc();
+    while is_space(peeked_char) {
+        get_char(chars).unwrap_or(EOF);
+        peeked_char = peek_char(chars);
     }
 
-    if is_alpha(last_char) {
+    if is_alpha(peeked_char) {
+        last_char = get_char(chars).unwrap_or(EOF);
+        peeked_char = peek_char(chars);
         identifier_str = last_char.to_string();
 
-        last_char = gc();
-        while is_alnum(last_char) {
+        while is_alnum(peeked_char) {
+            last_char = get_char(chars).unwrap_or(EOF);
+            peeked_char = peek_char(chars);
             identifier_str += &last_char.to_string();
-            last_char = gc();
         }
 
+        println!("identifier: \"{}\"", identifier_str);
         if identifier_str == "def" {
             return Token::TokDef as i32;
         }
         if identifier_str == "extern" {
             return Token::TokExtern as i32;
         }
-        println!("identifier: \"{}\"", identifier_str);
         return Token::TokIdentifier as i32;
     }
 
-    if is_digit(last_char) || last_char == '.' {
+    if is_digit(peeked_char) || peeked_char == '.' {
         // Number: [0-9.]+
         let mut num_str = "".to_string();
-        while is_digit(last_char) || last_char == '.' {
+        while is_digit(peeked_char) || peeked_char == '.' {
+            last_char = get_char(chars).unwrap_or(EOF);
+            peeked_char = peek_char(chars);
             num_str += &last_char.to_string();
-            last_char = gc();
         }
 
         num_val = match num_str.parse::<f64>() {
@@ -87,26 +99,27 @@ pub fn tokenize(chars: &mut str::Chars) -> i32 {
         return Token::TokNumber as i32;
     }
 
-    if last_char == '#' {
+    if peeked_char == '#' {
         // Comment until end of line.
-        while last_char != EOF && last_char != '\n' && last_char != '\r' {
-            last_char = gc();
+        while peeked_char != EOF && peeked_char != '\n' && peeked_char != '\r' {
+            get_char(chars).unwrap_or(EOF);
+            peeked_char = peek_char(chars);
         }
 
-        if last_char != EOF {
+        if peeked_char != EOF {
             // ここに入ることある？
             return tokenize(chars);
         }
     }
 
     // Check for end of file.  Don't eat the EOF.
-    if last_char == EOF {
+    if peeked_char == EOF {
         return Token::TokEof as i32;
     }
 
     // Otherwise, just return the character as its ascii value.
-    let this_char = last_char;
-    gc();
+    let this_char = peeked_char;
+    get_char(chars).unwrap_or(EOF);
 
     println!("token: \"{}\"", this_char);
     this_char.to_digit(10).unwrap_or_default() as i32
